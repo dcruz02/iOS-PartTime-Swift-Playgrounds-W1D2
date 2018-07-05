ed31e17e2e28df6e510d1142caa12eb9be6376db
Dennis <dcruz02@gmail.com>
2018-07-04 20:15:24 -0700

    Challenges completed

--- W1D2-ControlFlow.playground/Contents.swift
+++ W1D2-ControlFlow.playground/Contents.swift
@@ -49,6 +49,12 @@ if someThing == otherThing {
 
 bicycleCount = 1
 
+if bicycleCount == 1 {
+    print("There is \(bicycleCount) bicycle")
+} else {
+    print("There are \(bicycleCount) bicycles")
+}
+
 
 
 
@@ -58,7 +64,13 @@ bicycleCount = 1
 
 bicycleCount = 0
 
-
+if bicycleCount == 1 {
+    print("There is \(bicycleCount) bicycle")
+} else if bicycleCount == 0 {
+    print("There are \(bicycleCount)")
+}else {
+    print("There are \(bicycleCount) bicycles")
+}
 
 
 
@@ -68,9 +80,11 @@ bicycleCount = 0
 
 var inputString = "Do you like bicycles?"
 
-
-
-
+if inputString.hasSuffix("?"){
+    print("I think so")
+} else {
+    print("You don't say!")
+}
 
 
 
